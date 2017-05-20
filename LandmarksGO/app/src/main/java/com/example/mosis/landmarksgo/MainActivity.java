bf789434a4806fd773e630cc5b7d5b7fb1680b9b
Ivan Koma <ivankoma@outlook.com>
2017-05-20 18:51:43 +0200

    Changes to the Navigation Drawer
    Firebase username, email and photo is displayed.

--- LandmarksGO/app/src/main/java/com/example/mosis/landmarksgo/MainActivity.java
+++ LandmarksGO/app/src/main/java/com/example/mosis/landmarksgo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mosis.landmarksgo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -11,10 +12,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.mosis.landmarksgo.authentication.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity
 
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
+    private FirebaseUser user;
+    private static final String TAG = "LandmarksGO";
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +44,7 @@ public class MainActivity extends AppCompatActivity
         authListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
+                user = firebaseAuth.getCurrentUser();
                 if (user == null) {
                     // user auth state is changed - user is null
                     // launch login activity
@@ -64,8 +72,58 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        customizeUI();
+    }
+
+    private void customizeUI() {
+        Log.d(TAG, "MainActivity:changeUI:photoUrl started");
+        user = auth.getCurrentUser();
+        if(user!=null) {
+            Log.d(TAG, "MainActivity:changeUI: user!=null");
+
+            View headerView = navigationView.getHeaderView(0);
+
+            String displayName = user.getDisplayName();
+            String email = user.getEmail();
+
+            Log.d(TAG, "MainActivity:changeUI: displayName=" + displayName);
+            TextView profileName = (TextView) headerView.findViewById(R.id.textViewProfileName);
+            if(displayName!=null){
+                profileName.setText(displayName);
+            }else{
+                //there is no displayName when users is signed in with an email
+                profileName.setText("");
+            }
+
+            Log.d(TAG, "MainActivity:changeUI: email=" + email);
+            if(email!=null){
+                TextView profileEmail = (TextView) headerView.findViewById(R.id.textViewProfileEmail);
+                profileEmail.setText(email);
+            }
+
+            Uri photoUrl = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl();
+            Log.d(TAG, "MainActivity:changeUI: photoUrl=" + photoUrl);
+            ImageView profilePicture = (ImageView) headerView.findViewById(R.id.imageViewProfilePicture);
+
+            if(photoUrl==null){
+                Glide.with(this).load(R.drawable.empty_profile_picture).into(profilePicture);
+                //Glide.with(this).load("https://lintvwane.files.wordpress.com/2016/01/obama-guns_carr.jpg").into(profilePicture);
+                //Bitmap too large to be uploaded into a texture (5428x3698, max=4096x4096)
+                //We probably will probably get 50x50 pictures, so this won't be a problem
+            }else{
+                Glide.with(this).load(photoUrl).into(profilePicture);   //TODO: Test if this works. Add Facebook or Google as sign-in option
+            }
+            //TODO: Make picture round, not square.
+
+            final float scale = getResources().getDisplayMetrics().density;
+            int dpWidthInPx  = (int) (150 * scale);
+            int dpHeightInPx = (int) (150 * scale);
+            profilePicture.setMaxHeight(dpHeightInPx);
+            profilePicture.setMaxWidth(dpWidthInPx);
+        }
     }
 
     @Override
@@ -122,18 +180,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_map) {
+            //Intent intent = new Intent(MainActivity.this,MainActivity.class);
+            //startActivity(intent);
+        } else if (id == R.id.nav_friends){
+            Intent intent = new Intent(MainActivity.this,Friends.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_highscore){
+            Intent intent = new Intent(MainActivity.this,HighScore.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_settings){
+            Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_exit){
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
