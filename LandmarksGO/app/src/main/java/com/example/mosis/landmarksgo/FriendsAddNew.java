3290fd84521a11778cd2738a4a25fe7233ad23e2
Ivan Koma <ivankoma@outlook.com>
2017-05-20 16:31:26 +0200

    Added new activities
    MainActivity changed to a Navigation Drawer Activity; Added scroll view in Settings

---
+++ LandmarksGO/app/src/main/java/com/example/mosis/landmarksgo/FriendsAddNew.java
@@ -0,0 +1,13 @@
+package com.example.mosis.landmarksgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendsAddNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_add_new);
+    }
+}
